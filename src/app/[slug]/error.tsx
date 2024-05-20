3d30a0d735fb7d55573885dd5d5057b140ab75af
Muhammad Taufiq Febrianto <mtaufiqfebrianto6@gmail.com>
2024-05-20 18:08:05 +0700

    Handle errors

---
+++ src/app/[slug]/error.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+export default function Error() {
+	return (
+		<main className="mx-auto flex min-h-screen max-w-[50.625rem] items-center justify-center px-4 py-10 lg:px-0 lg:py-16">
+			<p className="text-center">
+				Something went wrong. Please try to refresh the page.
+			</p>
+		</main>
+	);
+}
---
+++ src/app/error.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+export default function Error() {
+	return (
+		<main className="mx-auto max-w-[50.625rem] px-4 py-6 lg:px-0">
+			<h1 className="text-center text-[2.5rem] font-bold leading-none">
+				Baseread
+			</h1>
+
+			<p className="mt-8 text-center">
+				Something went wrong. Please try to refresh the page.
+			</p>
+		</main>
+	);
+}
