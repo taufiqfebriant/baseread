200f1abd91a7e38db30cddd9869cf5f00cdf89e5
Muhammad Taufiq Febrianto <mtaufiqfebrianto6@gmail.com>
2024-05-21 11:07:38 +0700

    Add favicon & fix wordmark

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,11 +1,20 @@
+import Image from "next/image";
 import { PostListLoading } from "./post-list-loading";
 
 export default function Loading() {
 	return (
 		<main className="mx-auto max-w-[50.625rem] px-4 py-6 lg:px-0">
-			<h1 className="text-center text-[2.5rem] font-bold leading-none">
-				Baseread
-			</h1>
+			<div className="flex justify-center">
+				<div className="relative h-[30px] w-[179.17px]">
+					<Image
+						src="/wordmark.png"
+						alt="Baseread's wordmark"
+						fill
+						sizes="179.17px"
+						className="object-contain"
+					/>
+				</div>
+			</div>
 
 			<div className="mt-8 h-11 animate-pulse rounded bg-zinc-300" />
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -42,14 +42,16 @@ export default async function HomePage(props: Props) {
 
 	return (
 		<main className="mx-auto max-w-[50.625rem] px-4 py-6 lg:px-0">
-			<div className="relative h-[1.875rem] w-auto">
-				<Image
-					src="/wordmark.png"
-					alt="Baseread's wordmark"
-					fill
-					sizes="430px"
-					className="object-contain"
-				/>
+			<div className="flex justify-center">
+				<div className="relative h-[30px] w-[179.17px]">
+					<Image
+						src="/wordmark.png"
+						alt="Baseread's wordmark"
+						fill
+						sizes="179.17px"
+						className="object-contain"
+					/>
+				</div>
 			</div>
 
 			<Filter topics={topicsResult} />
